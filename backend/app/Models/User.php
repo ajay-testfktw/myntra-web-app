6c7f829114d558d366893c12791824aa2aa0bbbc
ppnavkiran <ppnavkiran@gmail.com>
2026-01-09 14:17:54 +0530

    User registration complete with jwt

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -40,6 +41,20 @@ class User extends Authenticatable
     {
         return [
             'password' => 'hashed',
+        ];
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'email' => $this->email,
+            'id' => $this->id,
+            'password' => $this->password,
         ];
     }
 }
--- backend/app/Services/UsermanagementService.php
+++ backend/app/Services/UsermanagementService.php
@@ -6,6 +6,7 @@ use App\Helper\ApiResponseHelper;
 use App\Models\User;
 use App\Repository\UsermanagementRepository;
 use Exception;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UsermanagementService
 {
@@ -18,9 +19,20 @@ class UsermanagementService
     public function register(array $data): User
     {
         try {
-            return $this->usermanagementRepository->register($data);
-        } catch (Exception $th) {
-            throw new Exception($th->getMessage());
+            $this->sendVerificationEmail($data['email']);
+            $user = $this->usermanagementRepository->register($data);
+            $user->access_token = $this->generateVerificationToken($user->email);
+            return $user;
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
         }
+    }
+
+    private function sendVerificationEmail(string $email): void {}
+
+    private function generateVerificationToken(string $email): string
+    {
+        $token = JWTAuth::fromUser(User::where('email', $email)->first());
+        return $token;
     }
 }
